52da1172ca04fb79b01e89f75476f1ce6932d834
Vũ Xuân Trường <119155820+wanwanvxt@users.noreply.github.com>
2024-08-24 13:43:25 +0700

    change bad request api handling

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -9,7 +9,7 @@ const routes = async (fastify: FastifyInstance, options: RegisterOptions) => {
     const query = decodeURIComponent((request.query as { query: string }).query)
     const page = (request.query as { page: number }).page || 1
 
-    if (!query || !page) {
+    if (!query || typeof page !== 'number') {
       reply.code(400).send()
       return reply
     }
@@ -116,7 +116,7 @@ const routes = async (fastify: FastifyInstance, options: RegisterOptions) => {
     const genre = decodeURIComponent((request.query as { genre: string }).genre)
     const page = (request.query as { page: number }).page || 1
 
-    if (!genre || !page) {
+    if (!genre || typeof page !== 'number') {
       reply.code(400).send()
       return reply
     }
